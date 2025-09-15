20e943c5ec66048a6378023ca18d3b0b66858ab7
elijah <eechan@cpp.edu>
2025-09-14 23:34:58 -0700

    Finalized decision tree & Output

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Assignment 1
-ML 4210 Assignment, building a decision tree
+## Understanding the Encoding
+What I've done is essentially map all of the values of each feature to a number. Here is how I have mapped it:
+
+- Age:
+  - Young = 0
+  - Prepresbyopic = 1
+  - Presbyopic = 2
+- Spectacle Prescription:
+  - Myopic = 0
+  - Hypermetropic = 1
+- Astigmatism:
+  - No = 0
+  - Yes = 1
+- Tear Production Rate:
+  - Reduced = 0
+  - Normal = 1
+
+## Colors and Directionals
+
+Something to note, if the color is of a shade of orange, it is the "Yes", or "True", direction of the parent boxes decision question. As for shades of blue, it is the "No", or "False", direction.
+
+For example, if the parent question is "Tear <= 0.5" the the orange shaded box that extends from it is the "Yes" answer to that question. The blue shaded box that extends from it is the "No" answer to that question.  
+
+In essence: Orange = True, Blue = False
+ 
+
+***
+With all of these in mind, when examining the outputted decision tree, it will make understanding the decision tree and how the decisions are made significantly easier. 
--- decision_tree.py
+++ decision_tree.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Elijah Chan
+# FILENAME: decision_tree.py
+# SPECIFICATION: reads csv and outputs a decision tree
 # FOR: CS 4210- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: ~ 4-5 hours
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
@@ -27,14 +27,33 @@ with open('contact_lens.csv', 'r') as csvfile:
 #encode the original categorical training features into numbers and add to the 4D array X.
 #--> add your Python code here
 # X =
+age_map = {"Young": 0, "Prepresbyopic": 1, "Presbyopic": 2}
+spectacle_map = {"Myope": 0, "Hypermetrope": 1}
+astig_map = {"No": 0, "Yes": 1}
+tear_map = {"Reduced": 0, "Normal": 1}
+
+for row in db:
+   X.append([
+      age_map[row[0]], 
+      spectacle_map[row[1]], 
+      astig_map[row[2]], 
+      tear_map[row[3]] 
+      ])
 
 #encode the original categorical training classes into numbers and add to the vector Y.
 #--> addd your Python code here
 # Y =
+lens_map = {"No": 0, "Yes": 1}
+
+for row in db:
+   Y.append(lens_map[row[4]])
+
 
 #fitting the decision tree to the data using entropy as your impurity measure
 #--> addd your Python code here
 #clf =
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+clf = clf.fit(X, Y)
 
 #plotting the decision tree
 tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes','No'], filled=True, rounded=True)
